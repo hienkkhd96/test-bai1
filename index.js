bb4f9c0a3d0cdbc4d2539ac7532628df993e9519
hienkkhd96 <hienkkhd@gmail.com>
2022-04-21 12:31:28 +0700

    update comment

--- index.js
+++ index.js
@@ -9,19 +9,23 @@ const rateWin = function (total) {
   let floor = 0;
   // Khởi tạo lần tung mỗi lượt chơi
   let count = 0;
+  let totalPlay = 0;
   //   Vì mỗi lần chơi ta được tung 100 lần lên số lần tung xúc sắc sẽ là bội của 100
   for (let i = 1; i <= total * 100; i++) {
     count += 1;
     //  Lấy ngẫu nhiên số điểm của xúc sắc
     let score = Math.random() * 6;
-    // Tại vì mỗi lượt chơi chỉ được gieo 100 lần=> Khi gieo hết 100 lần mà không win thì reset
+    // Tại vì mỗi lượt chơi chỉ được gieo 100 lần=> Khi gieo hết 100 lần mà không win thì reset và tính thêm 1 lượt chơi
     if (count > 100 && floor < 100) {
+      totalPlay += 1;
       floor = 0;
       count = 0;
     }
-    //  Nếu số tằng lớn hơn hoặc bằng 100 thì sẽ là 1 lần win
+    //  Nếu số tằng lớn hơn hoặc bằng 100 thì sẽ là 1 lần win,tính thêm 1 lượt và reset số tầng , số lượt tung còn lại về 0
     if (floor >= 100) {
+      totalPlay += 1;
       countWin = countWin + 1;
+      count = 0;
       floor = 0;
     }
     // Khi rơi vào trường hợp 0.1% phải đi lại từ tầng 0
@@ -47,9 +51,10 @@ const rateWin = function (total) {
   }
   //   Kết quả là số lần Win chia số lần chơi nhân 100
   //   Kết quả có đơn vị là phần trăm
-  result = (countWin / total) * 100;
+  result = (countWin / totalPlay) * 100;
+  console.log(totalPlay);
   return result;
 };
 
-console.log(rateWin(1000000));
-// Tỉ lệ win khoảng 8.4%(Chơi càng nhiều càng chính xác)
+console.log(rateWin(10000));
+// Tỉ lệ win khoảng 8.5%(Chơi càng nhiều càng chính xác)
